8a6cb1f0291daa1f343d0465f1d8d0067ec8aff9
develop <605750565@qq.com>
2017-07-07 17:28:33 +0800

    修改了按钮颜色，增加了 Toolbar标题栏并隐藏状态栏。

--- app/src/main/java/com/example/develop/machinedone/main/MainActivity.java
+++ app/src/main/java/com/example/develop/machinedone/main/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.develop.machinedone.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,16 +17,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow()
+//                .setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                        WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
         ListView list = findViewById(R.id.main_list);
         Button btn = findViewById(R.id.addlist);
+        toolbar.setTitle("");
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        setSupportActionBar(toolbar);
         btn.setOnClickListener(this);
     }
-
     @Override
     //添加数据到listview中
     public void onClick(View view)
     {
+        //点击事件 ，打开一个添加项目页面
+        //Intent intent=new Intent();
+        //startActivity(intent);
         Toast.makeText(this, "is ok", Toast.LENGTH_SHORT).show();
 
     }
