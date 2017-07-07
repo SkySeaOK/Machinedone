f85ffc90e10db5e35b009fe0825d27c16683a163
develop <605750565@qq.com>
2017-07-07 11:17:27 +0800

    添加了 listviewadapter

--- app/src/main/java/com/example/develop/machinedone/MainActivity.java
+++ app/src/main/java/com/example/develop/machinedone/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.develop.machinedone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //kaishi
         ListView list = findViewById(R.id.main_list);
+        Button btn = findViewById(R.id.addlist);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    //添加数据到listview中
+    public void onClick(View view)
+    {
+        Toast.makeText(this, "is ok", Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/com/example/develop/machinedone/adapter/MainListAdapter.java
@@ -0,0 +1,36 @@
+package com.example.develop.machinedone.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by develop on 2017/7/7.
+ */
+
+public class MainListAdapter extends BaseAdapter
+{
+    public MainListAdapter()//预留传参
+    {
+
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
