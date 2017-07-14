61ba564d2c4c90965699c4a28d5bec23aa521e82
develop <605750565@qq.com>
2017-07-14 09:32:27 +0800

    AllProject和MainProject之间的数据链接

--- app/src/main/java/com/example/develop/machinedone/main/MainProjectActivity.java
+++ app/src/main/java/com/example/develop/machinedone/main/MainProjectActivity.java
@@ -1,5 +1,6 @@
 package com.example.develop.machinedone.main;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,24 +12,64 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.develop.machinedone.R;
+import com.example.develop.machinedone.api.ApiService;
+import com.example.develop.machinedone.bean.MainList;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainProjectActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SearchView sv;
+    private   String url="http://192.168.14.2:8080/";
+    private int position;
+    private TextView projectTitle;
+    private TextView projectUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_project);
+        projectTitle = findViewById(R.id.project_title);
+        projectUser = findViewById(R.id.project_user);
+
         ImageView toolbarImg = findViewById(R.id.toolbar_img);
         ImageView toolbarBack = findViewById(R.id.toolbar_back);
         TextView username = findViewById(R.id.toolbar_username);
         toolbarImg.setImageResource(R.mipmap.ic_launcher);
         toolbarBack.setImageResource(R.mipmap.ic_title_back);
         sv = findViewById(R.id.searchView);
+        //通过getIntent获取AllProject传递过来的position下标
+        Intent intent = getIntent();
+        position = intent.getIntExtra("position", 0);
         username.setText("User");
+        //通过retrofit获取网络数据，通过传递过来的position去提取对应的数据
+        Retrofit build = new Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
+        ApiService apiService = build.create(ApiService.class);
+        Call<MainList> list = apiService.getList();
+        list.enqueue(new Callback<MainList>() {
+            @Override
+            public void onResponse(Call<MainList> call, Response<MainList> response) {
+
+                List<MainList.MenuitemBean> menuitem = response.body().getMenuitem();
+                MainList.MenuitemBean menuitemBean = menuitem.get(position);
+                projectTitle.setText(menuitemBean.getValue());
+                projectUser.setText(menuitemBean.getOnclick()+"  创建");
+
+            }
+
+            @Override
+            public void onFailure(Call<MainList> call, Throwable t) {
+
+            }
+        });
         // setSupportActionBar(toolbar);
         toolbarImg.setOnClickListener(this);
         toolbarBack.setOnClickListener(this);
