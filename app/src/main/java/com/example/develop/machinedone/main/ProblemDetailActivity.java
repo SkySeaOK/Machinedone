93c9472c70f859e1d9b669c2d1f36cbd50be1fb2
shiyid <15280216549@163.com>
2017-08-07 18:11:34 +0800

    为问题详情页面点击操作按钮上拉列表中的拒绝按钮添加了拒绝功能

--- app/src/main/java/com/example/develop/machinedone/main/ProblemDetailActivity.java
+++ app/src/main/java/com/example/develop/machinedone/main/ProblemDetailActivity.java
@@ -36,10 +36,14 @@ public class ProblemDetailActivity extends AppCompatActivity implements View.OnC
     private PopupWindow operate_popupWindow;
     private PopupWindow assign_popupWindow;
     private PopupWindow handle_popupWindow;
+    private PopupWindow refuse_popupWindow;
+    private PopupWindow refuseReason_popupWindow;
     private Button operate_button;
     private Button operate_btn;
     private TextView logbtn;
     private boolean x = false;
+    private boolean y = false;
+    private boolean z = false;
     private ListView log_listview;
     private List<LogListViewItem.MenuitemBean> menuitemBeans = new ArrayList<>();
     private LogListViewAdapter logListViewAdapter;
@@ -52,6 +56,13 @@ public class ProblemDetailActivity extends AppCompatActivity implements View.OnC
     private LinearLayout handle_user;
     private TextView handleUser_text;
     private TextView user_text;
+    private TextView assign_person;
+    private ImageView refuseDelete_img;
+    private LinearLayout refuseHandle_liner;
+    private TextView refuseHandle_text;
+    private TextView refuse_title;
+    private LinearLayout refuse_reason;
+    private View v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -118,6 +129,16 @@ public class ProblemDetailActivity extends AppCompatActivity implements View.OnC
                 break;
             case R.id.assign_btn:
                 showAssignPopupWindow();
+                assign_person.setText("指派新的处理人");
+                assign_liner.setBackgroundColor(0xFFBDBDBD);
+                delay_liner.setBackgroundColor(0xFFBDBDBD);
+                delete_liner.setBackgroundColor(0xFFBDBDBD);
+                operate_btn.setBackgroundColor(0xFFBDBDBD);
+                operateList_liner.setBackgroundColor(0xFFBBBBBB);
+                break;
+            case R.id.finish_btn:
+                showAssignPopupWindow();
+                assign_person.setText("完成");
                 assign_liner.setBackgroundColor(0xFFBDBDBD);
                 delay_liner.setBackgroundColor(0xFFBDBDBD);
                 delete_liner.setBackgroundColor(0xFFBDBDBD);
@@ -136,12 +157,49 @@ public class ProblemDetailActivity extends AppCompatActivity implements View.OnC
                 else
                 {
                     showHandlePopupWindow();
+                    handle_popupWindow.showAtLocation(v,Gravity.CENTER,50, 20);
                     x = true;
                 }
                 break;
             case R.id.handle_user:
                 handleUser_text.setText(user_text.getText());
                 handle_popupWindow.dismiss();
+                break;
+            case R.id.refuse_btn:
+                showRefusePopupWindow();
+                assign_liner.setBackgroundColor(0xFFBDBDBD);
+                delay_liner.setBackgroundColor(0xFFBDBDBD);
+                delete_liner.setBackgroundColor(0xFFBDBDBD);
+                operate_btn.setBackgroundColor(0xFFBDBDBD);
+                operateList_liner.setBackgroundColor(0xFFBBBBBB);
+                break;
+            case R.id.refuseDelete_img:
+                refuse_popupWindow.dismiss();
+                break;
+            case R.id.refuseHandle_liner:
+                if(y)
+                {
+                    handle_popupWindow.dismiss();
+                    y = false;
+                }
+                else
+                {
+                    showHandlePopupWindow();
+                    handle_popupWindow.showAtLocation(v,Gravity.CENTER,50, 50);
+                    y = true;
+                }
+                break;
+            case R.id.refuse_reason:
+                if(z)
+                {
+                    refuseReason_popupWindow.dismiss();
+                    z = false;
+                }
+                else
+                {
+                    showRefuseReasonPopupWindow();
+                    z = true;
+                }
                 break;
         }
     }
@@ -183,7 +241,11 @@ public class ProblemDetailActivity extends AppCompatActivity implements View.OnC
         operate_btn = contentView.findViewById(R.id.operate_btn);
         operate_btn.setOnClickListener(this);
         TextView assign_btn = contentView.findViewById(R.id.assign_btn);
+        TextView finish_btn = contentView.findViewById(R.id.finish_btn);
+        TextView refuse_btn = contentView.findViewById(R.id.refuse_btn);
         assign_btn.setOnClickListener(this);
+        finish_btn.setOnClickListener(this);
+        refuse_btn.setOnClickListener(this);
         assign_liner = contentView.findViewById(R.id.assign_liner);
         delay_liner = contentView.findViewById(R.id.delay_liner);
         delete_liner = contentView.findViewById(R.id.delete_liner);
@@ -227,17 +289,71 @@ public class ProblemDetailActivity extends AppCompatActivity implements View.OnC
         delete_img.setOnClickListener(this);
         handle_liner.setOnClickListener(this);
         handleUser_text = contentView.findViewById(R.id.handleUser_text);
+        assign_person = contentView.findViewById(R.id.assign_person);
+    }
+
+    private void showRefusePopupWindow() {
+        View contentView = LayoutInflater.from(ProblemDetailActivity.this).inflate(R.layout.refuse_page, null);
+        contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        refuse_popupWindow = new PopupWindow(contentView, 600, 0);
+        refuse_popupWindow.setBackgroundDrawable(new BitmapDrawable());//注意这里如果不设置，下面的setOutsideTouchable(true);允许点击外部消失会失效
+        refuse_popupWindow.setOutsideTouchable(true);   //设置外部点击关闭ppw窗口
+        refuse_popupWindow.setFocusable(true);
+        refuse_popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        refuse_popupWindow.showAtLocation(contentView, Gravity.CENTER, 0, 0);
+        WindowManager.LayoutParams params = ProblemDetailActivity.this.getWindow().getAttributes();
+        //当弹出Popupwindow时，背景变半透明
+        params.alpha = 0.5f;
+        ProblemDetailActivity.this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        ProblemDetailActivity.this.getWindow().setAttributes(params);
+        //设置Popupwindow关闭监听，当Popupwindow关闭，背景恢复0.7f
+        refuse_popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                WindowManager.LayoutParams params = ProblemDetailActivity.this.getWindow().getAttributes();
+                params.alpha = 0.7f;
+                ProblemDetailActivity.this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                ProblemDetailActivity.this.getWindow().setAttributes(params);
+                assign_liner.setBackgroundColor(0xFFffffff);
+                delay_liner.setBackgroundColor(0xFFffffff);
+                delete_liner.setBackgroundColor(0xFFffffff);
+                operate_btn.setBackgroundColor(0xFFffffff);
+                operateList_liner.setBackgroundColor(0xFFF4F4F4);
+            }
+        });
+        refuse_popupWindow.setAnimationStyle(R.style.AnimationFade);
+        refuse_popupWindow.update();
+        refuseDelete_img = contentView.findViewById(R.id.refuseDelete_img);
+        refuseHandle_liner = contentView.findViewById(R.id.refuseHandle_liner);
+        refuseDelete_img.setOnClickListener(this);
+        refuseHandle_liner.setOnClickListener(this);
+        refuseHandle_text = contentView.findViewById(R.id.refuseHandle_text);
+        refuse_reason = contentView.findViewById(R.id.refuse_reason);
+        refuse_reason.setOnClickListener(this);
     }
 
     private void showHandlePopupWindow() {
-        View contentView = LayoutInflater.from(ProblemDetailActivity.this).inflate(R.layout.handle_person, null);
-        handle_popupWindow = new PopupWindow(contentView, 400, 300);
+        v = LayoutInflater.from(ProblemDetailActivity.this).inflate(R.layout.handle_person, null);
+        handle_popupWindow = new PopupWindow(v, 400, 300);
         handle_popupWindow.setOutsideTouchable(true);
-        handle_popupWindow.showAtLocation(contentView,Gravity.CENTER,50, 20);
+//        handle_popupWindow.showAtLocation(v,Gravity.CENTER,50, 20);
         handle_popupWindow.setFocusable(true);
-        handle_user = contentView.findViewById(R.id.handle_user);
+        handle_user = v.findViewById(R.id.handle_user);
         handle_user.setOnClickListener(this);
-        user_text = contentView.findViewById(R.id.user_text);
+        user_text = v.findViewById(R.id.user_text);
+
+    }
+
+    private void showRefuseReasonPopupWindow() {
+        View contentView = LayoutInflater.from(ProblemDetailActivity.this).inflate(R.layout.refuse_reason, null);
+        refuseReason_popupWindow = new PopupWindow(contentView, 400, 0);
+        refuseReason_popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        refuseReason_popupWindow.setOutsideTouchable(true);
+        refuseReason_popupWindow.showAtLocation(contentView,Gravity.CENTER,50, 70);
+        refuseReason_popupWindow.setFocusable(true);
+//        handle_user = contentView.findViewById(R.id.handle_user);
+//        handle_user.setOnClickListener(this);
+//        user_text = contentView.findViewById(R.id.user_text);
 
     }
 }
